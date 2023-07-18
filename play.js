dd126040d0a4582a693a38cdb9b3d5f50a1e069e
Taha Mulla <tahamulla@hotmail.com>
2023-07-18 19:58:56 -0400

    Removing the test console.log

--- play.js
+++ play.js
@@ -1,9 +1,6 @@
 const { connect } = require("./client.js");
 const { setupInput } = require('./input');
 
-console.log(connect);
 console.log("Connecting ...");
-
 const conn = connect();
 setupInput(conn);
-console.log(setupInput);
