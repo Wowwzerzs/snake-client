a428229029eb04cde9e8902a7e7a4231ff4de439
Taha Mulla <tahamulla@hotmail.com>
2023-07-18 19:13:11 -0400

    Adding a sending message feature

--- input.js
+++ input.js
@@ -2,17 +2,23 @@ let connection;
 
 // Handle user input from stdin
 const handleUserInput = function (key) {
-  if (key === '\u0003') {
+  if (key === "\u0003") {
     process.exit();
   } else if (connection) {
-    if (key === 'w') {
+    if (key === "w") {
       connection.write("Move: up");
-    } else if (key === 'a') {
+    } else if (key === "a") {
       connection.write("Move: left");
-    } else if (key === 's') {
+    } else if (key === "s") {
       connection.write("Move: down");
-    } else if (key === 'd') {
+    } else if (key === "d") {
       connection.write("Move: right");
+    } else if (key === "b") {
+      connection.write("Say: Eat My Dust!");
+    } else if (key === "n") {
+      connection.write("Say: OHH NOO!");
+    } else if (key === "m") {
+      connection.write("Say: GG");
     }
   }
 };
