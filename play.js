a660e12e4a243ff5ebce56ac500d496720132f5f
Taha Mulla <tahamulla@hotmail.com>
2023-07-18 18:18:05 -0400

    New js file on managing the user input

--- play.js
+++ play.js
@@ -1,26 +1,8 @@
 const { connect } = require("./client.js");
+const { setupInput } = require('./input');
 
 console.log(connect);
 console.log("Connecting ...");
 connect();
-
-// Setup interface to handle user input from stdin
-
-const setupInput = function () {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-  stdin.on("data", handleUserInput);
-  return stdin;
-};
-
-const handleUserInput = function (key) {
-  // \u0003 maps to ctrl+c input
-  if (key === '\u0003') {
-    process.exit();
-  }
-};
-
-// Call setupInput to start listening for user input
+console.log(setupInput);
 setupInput();
