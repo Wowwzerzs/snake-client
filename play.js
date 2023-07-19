99500bdee5105cdd6118a87b39ddcdc17f59458e
Taha Mulla <tahamulla@hotmail.com>
2023-07-18 20:37:29 -0400

    update

--- play.js
+++ play.js
@@ -4,12 +4,3 @@ const { setupInput } = require('./input');
 console.log("Connecting ...");
 const conn = connect();
 setupInput(conn);
-
-
-conn.on("newUserJoined", (message) => {
-  sendNewUserMessage(message);
-});
-
-const sendNewUserMessage = (message) => {
-  console.log("New user joined:", message);
-};
